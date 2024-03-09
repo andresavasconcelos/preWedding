0603852b6e0ab6c05701692b5e5493fc25577daf
andresa.vasconcelos <andresa.vasconcelos-30@hotmail.com>
2024-03-08 21:19:02 -0300

    ajuste page mdrinhas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,12 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'madrinhas',component:MadrinhaComponent},
-  {path:'padrinhos',component:PadrinhoComponent},
-  {path:'menu',component:MenuComponent},
-  {path:'about',component:AboutComponent},
-  {path:'contact',component:ContactComponent}
+  {path:'', component:HomeComponent},
+  {path:'madrinhas', component:MadrinhaComponent},
+  {path:'padrinhos', component:PadrinhoComponent},
+  {path:'menu', component:MenuComponent},
+  {path:'about', component:AboutComponent},
+  {path:'contact', component:ContactComponent}
 ];
 
 @NgModule({
