0169ce0bac30ecc05ec59c90de416e61c6389151
andresa.vasconcelos <andresa.vasconcelos-30@hotmail.com>
2024-07-08 00:04:06 -0300

    -

--- src/app/pages/opt-image-padrinhos/opt-image-padrinhos.component.ts
+++ src/app/pages/opt-image-padrinhos/opt-image-padrinhos.component.ts
@@ -8,5 +8,46 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
   styleUrls: ['./opt-image-padrinhos.component.css']
 })
 export class OptImagePadrinhosComponent {
+  images: string[] = [
+    '../../assets/img/padrinhos/ref1.png',
+    '../../assets/img/padrinhos/ref2.png',
+    '../../assets/img/padrinhos/ref3.png',
+    '../../assets/img/padrinhos/ref4.png',
+    '../../assets/img/padrinhos/ref5.png',
+    '../../assets/img/padrinhos/ref6.png'
+  ];
 
+  selectedImage: string | null = null;
+  currentIndex: number = 0;
+
+  openModal(image: string) {
+    this.currentIndex = this.images.indexOf(image);
+    this.selectedImage = image;
+  }
+
+  closeModal() {
+    this.selectedImage = null;
+  }
+
+  prevImage(event: MouseEvent) {
+    event.stopPropagation();
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.selectedImage = this.images[this.currentIndex];
+    } else {
+      this.currentIndex = this.images.length - 1;
+      this.selectedImage = this.images[this.currentIndex];
+    }
+  }
+
+  nextImage(event: MouseEvent) {
+    event.stopPropagation();
+    if (this.currentIndex < this.images.length - 1) {
+      this.currentIndex++;
+      this.selectedImage = this.images[this.currentIndex];
+    } else {
+      this.currentIndex = 0;
+      this.selectedImage = this.images[this.currentIndex];
+    }
+  }
 }
